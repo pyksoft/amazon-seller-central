09fb0dd4c213856b0595ca91121c964b797ae844
pyksoft <pyksoft@gmail.com>
2014-10-14 21:25:46 +0300

    Fix notification order errors

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,9 +3,9 @@ class Notification < ActiveRecord::Base
 
   def self.sorted_notifications
     unseen_notifications = Notification.where('seen is null OR seen = false')
-    p unseen_notifications.select do |notification|
+    p(unseen_notifications.select do |notification|
       notification.change_title && notification.change_title.include?('price')
-    end.map{|a| a.change_title.delete('_price').to_f}
+    end.map{|notification| notification.change_title.delete('_price').to_f})
     sorted_notifications = unseen_notifications.select do |notification|
       notification.change_title && notification.change_title.include?('price')
     end.sort_by do |notification|
