35ca0bdb36ead390e7c6de79b407b84dc9a9671a
pyksoft <pyksoft@gmail.com>
2014-10-14 21:12:00 +0300

    Fix notification order errors2.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -7,7 +7,7 @@ class Notification < ActiveRecord::Base
       notification.change_title && notification.change_title.include?('price')
     end.sort_by do |notification|
       notification.change_title.delete('_price').to_f
-    end.reverse
+    end
     (unseen_notifications - sorted_notifications).sort_by do |notification|
       case notification.change_title
         when 'amazon_unavailable'
