a0bd4ff034aa577f2722753beff2a44fc8709885
pyksoft <pyksoft@gmail.com>
2014-10-22 00:31:28 +0300

    Remove validate prime on create.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ActiveRecord::Base
   validates_uniqueness_of :ebay_item_id, :amazon_asin_number
   validates_presence_of :ebay_item_id, :amazon_asin_number
   validate :ebay_item_validation, :amazon_asin_number_validation
-  validate :prime_validation, :on => :create
+  # validate :prime_validation, :on => :create
 
   @@thread_compare_working = false
   @@working_count = 1
