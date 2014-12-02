e977e15ea5b3c3886d4a96e0cfd5d9b16044ad50
pyksoft <pyksoft@gmail.com>
2014-12-02 22:35:42 +0200

    Add product title to notification.

--- db/migrate/20141202201341_add_notifications_columns_amazon_ebay.rb
+++ db/migrate/20141202201341_add_notifications_columns_amazon_ebay.rb
@@ -1,7 +1,7 @@
 class AddNotificationsColumnsAmazonEbay < ActiveRecord::Migration
   def change
     change_table :notifications do |t|
-      t.string :amazon_asin_number,:ebay_item_id
+      t.string :amazon_asin_number,:ebay_item_id,:title
     end
   end
 end
