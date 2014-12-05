d746d9acccb139d6152a18f1da8b609e2335a4c5
pyksoft <pyksoft@gmail.com>
2014-12-05 17:53:59 +0200

    Change working_count

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ActiveRecord::Base
 
   @@test_workspace = Rails.env == 'development'
   @@thread_compare_working = false
-  @@working_count = 0
+  @@working_count = 1
 
   def self.test_workspace
     @@test_workspace
