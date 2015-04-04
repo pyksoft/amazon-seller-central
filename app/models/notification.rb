a9d172d160b3c5ed4f0872c2a85b21aa692dcd5e
pyksoft <pyksoft@gmail.com>
2015-04-05 00:29:55 +0300

    Fix small bug with ebay url.

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -51,6 +51,7 @@ class Notification < ActiveRecord::Base
           arr << notification.values_at(%i[id text image_url row_css change_title skip_accepted]).merge(
               :product => {
                   :title => notification.title || notification.product.try(:title),
+                  :ebay_item_id => notification.ebay_item_id || notification.product.try(:ebay_item_id),
                   :amazon_asin_number => notification.amazon_asin_number || notification.product.try(:amazon_asin_number),
                   :amazon_url => notification.product.try(:item_url) || Product.new(notification.values_at(:amazon_asin_number)).item_url
               }
